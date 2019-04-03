bb141b2aad7021b38cda93b423f96c222a8fbc32
Adrien Chesneau <achesne@a-018818.ups.u-psud.fr>
2019-04-03 11:48:47 +0200

    plus joli

--- projet.py
+++ projet.py
@@ -2,6 +2,8 @@ import json
 import os
 import sys
 import numpy as np
+
+
 data_path = './corpus/'
 fr_path = data_path + 'fr/'
 en_path = data_path + 'en/'
@@ -11,16 +13,22 @@ en_diles = os.listdir(en_path)
 
 fr_json = dict()
 
-for filename in fr_files :
-    path = fr_path + filename
-    j = json.load(open(path))
-    arr = np.array(j)
-    fr_json[filename] = arr
-    print(filename, '\t', arr.shape)
+#Calcule la taille de chaque data 
+def corpus_size():
+    print("Nombre de phrase des differents corpus:")
+    for filename in fr_files :
+        path = fr_path + filename
+        j = json.load(open(path))
+        arr = np.array(j)
+        fr_json[filename] = arr
+        if(arr.shape != ()):
+            print(filename, ':', arr.shape[0])
+        else:
+            print(filename, ': 0')
 
-def calcul_ooc(train_file, test_file):
+#Calcule le pourcentage de mot donnés dans les datas test qui ne sont pas contenus dans leur train
+def calcul_oov(train_file, test_file):
     vocab_train = set()
-    # 1 - construction du vocab
     for i in train_file:
         for j in i[0]:
             vocab_train.add(j)
@@ -32,35 +40,17 @@ def calcul_ooc(train_file, test_file):
                 nb_out_vocab += 1
     return (nb_out_vocab / (len(vocab_train) + nb_out_vocab)) * 100
 
-def calcul_kl_divergence():
+##def calcul_kl_divergence():
     
-    
-gsd_beg = 'fr.gsd.'
-gsd_train = fr_json[gsd_beg + 'train.json']
-gsd_test = fr_json[gsd_beg + 'test.json']
-print(calcul_ooc(gsd_train, gsd_test))
+def calcul_oov_test():
+    data = ['fr.gsd.','fr.partut.','fr.sequoia.','fr.spoken.','fr.ftb.','fr.pud.'] 
+    print("Pourcentage d'OOV des differents corpus:")  
+    for i in data:
+        train = fr_json[i+'train.json']
+        test = fr_json[i+'test.json']
+        name = i.split('.')
+        print(name[1],':',calcul_oov(train,test),'%')
 
-partut_beg = 'fr.partut.'
-partut_train = fr_json[partut_beg + 'train.json']
-partut_test = fr_json[partut_beg + 'test.json']
-print('partut\t', calcul_ooc(partut_train, partut_test))
-
-sequoia_beg = 'fr.sequoia.'
-sequoia_train = fr_json[sequoia_beg + 'train.json']
-sequoia_test = fr_json[sequoia_beg + 'test.json']
-print('sequoia\t', calcul_ooc(sequoia_train, sequoia_test))
-
-spoken_beg = 'fr.spoken.'
-spoken_train = fr_json[spoken_beg + 'train.json']
-spoken_test = fr_json[spoken_beg + 'test.json']
-print('spoken\t', calcul_ooc(spoken_train, spoken_test))
-
-ftb_beg = 'fr.ftb.'
-ftb_train = fr_json[ftb_beg + 'train.json']
-ftb_test = fr_json[ftb_beg + 'test.json']
-print('ftb\t', calcul_ooc(ftb_train, ftb_test))
-
-pud_beg = 'fr.pud.'
-pud_train = fr_json[pud_beg + 'train.json']
-pud_test = fr_json[pud_beg + 'test.json']
-print('pud\t', calcul_ooc(pud_train, pud_test))
+corpus_size()
+print()
+calcul_oov_test()
